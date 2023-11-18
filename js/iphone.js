e5c37098c6ad0c6d42da98c765eebadf6a444631
ibrahim omer <himaomer6@gmail.com>
2023-11-18 14:14:05 +0300

    update from pc

---
+++ js/iphone.js
@@ -0,0 +1,16 @@
+let header=document.querySelector(".section-img img")
+
+// $(document).ready(function(){
+//    $(".section-img > img").click(function(){
+    
+//    });
+  
+                               
+    $(".nav-foot img").click(function(){
+     let src= this.src;
+     header.src=src;
+        
+        
+        
+        // $(".section-img img").css("src",src);
+    })
